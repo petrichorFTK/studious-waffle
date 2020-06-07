9d7fb74fce98fe0c4116bed1c20ed290662781cd
petrichorFTK <64805777+petrichorFTK@users.noreply.github.com>
2020-06-06 21:54:23 -0500

    Add files via upload
    Very simple port scanner in Python3.

---
+++ scanner.py
@@ -0,0 +1,15 @@
+import nmap
+import sys
+
+target = str(sys.argv[1])
+ports = [21,22,80,443,445,8080]
+
+scan_v = nmap.PortScanner()
+
+print("\nScanning",target,"ports 21,22,80,443,445,8080...\n")
+
+for port in ports:
+    portscan = scan_v.scan(target,str(port))
+    print("Port",port," is ",portscan['scan'][target]['tcp'][port]['state'])
+
+print("\nHost",target," is ",portscan['scan'][target]['status']['state'])
